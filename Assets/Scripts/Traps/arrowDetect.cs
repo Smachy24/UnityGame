0837d9606861df1d9dea7fe495983b913c21ce06
Val <pro.mvaldo@gmail.com>
2023-03-30 00:50:20 +0200

    some effects

---
+++ Assets/Scripts/Traps/arrowDetect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowDetect : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
