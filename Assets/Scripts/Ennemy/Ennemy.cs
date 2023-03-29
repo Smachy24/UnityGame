9a69c95ae48bcea868d9f55d3419f90fb92693ad
Smachy <pro.diegocdossantos@gmail.com>
2023-03-29 10:51:35 +0200

    movement ennemy

---
+++ Assets/Scripts/Ennemy/Ennemy.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+
+public class Ennemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private float maxPv;
+
+    [SerializeField]
+    private float currentPv;
+
+    private Rigidbody2D _rigBod;
+
+    [SerializeField]
+    private LayerMask m_GroundLayer;
+
+    [SerializeField]
+    private float m_JumpForce;
+
+    [SerializeField]
+    private float m_MoveSpeed;
+
+    private float _movement;
+    private float _movement2;
+    private bool _isJump;
+
+    private static string _direction = "right";
+    public static string direction
+    {
+        get
+        {
+            return _direction;
+        }
+        set
+        {
+            _direction = value;
+        }
+
+    }
+
+
+    private void Awake()
+    {
+        _rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Physics2D.Raycast(transform.position, Vector2.down, 1f, m_GroundLayer))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+               
+                _isJump = true;
+            }
+        }*/
+
+        _movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        /*if (_isJump)
+        {
+            _isJump = false;
+
+            _rigBod.AddForce(Vector2.up * m_JumpForce, ForceMode2D.Impulse);
+        }*/
+        Debug.Log(_movement);
+        if(_direction == "right")
+        {
+            _movement2 = 1;
+        }
+        else
+        {
+            _movement2 = -1;
+        }
+
+        _rigBod.velocity = new Vector2(m_MoveSpeed * _movement2, _rigBod.velocity.y);
+    }
+
+    public void Stop()
+    {
+
+        _rigBod.velocity = new Vector2(0, _rigBod.velocity.y);
+        enabled = false;
+    }
+}
---
+++ Assets/Scripts/GameElements/EnnemyLimit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyLimit : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ennemy"))
+        {
+            
+            if (Ennemy.direction == "right")
+            {
+                Ennemy.direction = "left";
+            }
+            else if(Ennemy.direction == "left")
+            {
+                Ennemy.direction = "right";
+            }
+            Debug.Log(Ennemy.direction);
+
+        }
+    }
+
+}
