5f02ae2ab8f9811c273a34adbf84220ffe880404
Smachy <pro.diegocdossantos@gmail.com>
2023-03-30 16:53:20 +0200

    push

--- Assets/Scripts/General/MenuManager.cs
+++ Assets/Scripts/General/MenuManager.cs
@@ -14,7 +14,7 @@ public class MenuManager : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void SettingButton()
