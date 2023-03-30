ad6d7b0476309c35a2120f7def22162c3af98df3
jeff <jeff@gmail.com>
2023-03-30 13:02:02 +0200

    sound effect system + fix slider

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -2,10 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider soundSlider;
+
+    private void Start()
+    {
+        audioMixer.GetFloat("Music", out float musicVolumeForSlider);
+        musicSlider.value = musicVolumeForSlider;
+
+        audioMixer.GetFloat("Sound", out float soundVolumeForSlider);
+        soundSlider.value = soundVolumeForSlider;
+    }
 
     public void setVolume(float volume)
     {
